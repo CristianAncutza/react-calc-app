5453dad370502a16be6fc2352970ee0a502f4887
Cristian <cristianancutza@gmail.com>
2021-06-10 13:27:41 -0300

    Add files via upload
    commit inicial

---
+++ src/components/Result.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Result = () => {
+    debugger
+    return (
+        <div className="result">
+            <span>25</span>
+        </div>
+    )
+}
+
+export default Result
---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+//importar el componente
+import App from './App'
+
+
+//utilizar el componente
+ReactDOM.render(<App />,
+  document.getElementById('root')
+  )
+
